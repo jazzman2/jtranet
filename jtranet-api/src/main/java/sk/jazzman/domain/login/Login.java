910f9e8c0ab6962488e45962e7973874c2064429
jano <kovalcikj@gmail.com>
2017-03-06 22:13:13 +0100

    refs #1 - create module api, login-frontend,login-backend

---
+++ jtranet-api/src/main/java/sk/jazzman/domain/login/Login.java
@@ -0,0 +1,18 @@
+package sk.jazzman.domain.login;
+
+/**
+ * Login entity
+ *
+ * @author Jan Kovalcik
+ * @since v0.0.1
+ */
+public interface Login {
+    public String getLogin();
+
+    public void setLogin(String login);
+
+    public String getPassword();
+
+    public void setPassword(String password);
+
+}
